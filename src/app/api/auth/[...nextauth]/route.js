9edd8e092ea9b7893edbc94008799dce66019b6b
Pritamghara <pritamghara13@gmail.com>
2024-06-02 15:23:59 +0530

    add mini profile to sidebar

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -12,5 +12,12 @@ const handler=NextAuth({
         })
 
     ],
+    callbacks:{
+        async session({session,token}){
+            session.user.username=session.user.name.split(' ').join('').toLocaleLowerCase()
+            session.user.uid=token.sub
+            return session
+        }
+    }
 });
 export {handler as GET,handler as POST}
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,9 @@
 import React from 'react'
 import { FaSquareXTwitter } from "react-icons/fa6";
 import  Link  from 'next/link';
-import { HiHome } from "react-icons/hi";
+import { HiHome,HiDotsHorizontal } from "react-icons/hi";
+
+
 
 import { useSession } from 'next-auth/react';
 
@@ -11,8 +13,11 @@ import { signIn, signOut } from 'next-auth/react';
 const Sidebar = () => {
 
   const {data:session}=useSession()
+
   return (
-    <div className='flex flex-col gap-5 p-3 ml-5'>
+    <div className='flex flex-col p-3 justify-between h-screen'>
+      <div className='flex flex-col gap-3'>
+
 
         <Link href='/'>
             <FaSquareXTwitter className='w-20 h-20 cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-200'/>
@@ -29,13 +34,28 @@ const Sidebar = () => {
               Sign Out
            </button>
 
-           ):(
-            <button onClick={()=>signIn()} className='bg-blue-400 text-white  rounded-full  hover:brightness-95 transition-all duration-200 w-60 h-9 shadow-md hidden xl:inline font:semibold'>
+):(
+  <button onClick={()=>signIn()} className='bg-blue-400 text-white  rounded-full  hover:brightness-95 transition-all duration-200 w-60 h-9 shadow-md hidden xl:inline font:semibold'>
               Sign In
            </button>
 
-           )}
+)}
         
+       </div>
+       {
+        session && (
+          <div className='text-gray-700 text-sm flex items-center cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-all duration-200'>
+            <img src={session.user.image} alt="user-img" className='h-10 w-10 rounded-full xl:mr-2' />
+            <div className='hidden xl:inline'>
+              <h4 className='font-bold'>{session.user.name}</h4>
+              <p className='text-gray-500'>{session.user.username}</p>
+            </div>
+            <div>
+              <HiDotsHorizontal className=' h-7 xl:ml-8 hidden xl:inline'/>
+            </div>
+          </div>
+        )
+       }
     </div>
   )
 }
