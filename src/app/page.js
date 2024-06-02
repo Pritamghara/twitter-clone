59b5c53faf9901c9a72bf2dda4a32c597c7ce29a
Pritamghara <pritamghara13@gmail.com>
2024-06-02 13:10:31 +0530

    "third commit"

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='text-red-700'>page</div>
+    <div className='text-red-700'>Home page</div>
   )
 }
 
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { FaSquareXTwitter } from "react-icons/fa6";
+import  Link  from 'next/link';
+import { HiHome } from "react-icons/hi";
+const Sidebar = () => {
+  return (
+    <div className='flex flex-col gap-5 p-3 ml-5'>
+
+        <Link href='/'>
+            <FaSquareXTwitter className='w-20 h-20 cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-200'/>
+
+        </Link>
+           <Link href='/' className='flex items-center p-3 hover:bg-gray-100 rounded-full transition-all duration-200 gap-2 w-fit'>
+
+           <HiHome className='w-12 h-12' />
+           <span className='font-bold hidden xl:inline'>Home</span>
+           
+           
+           </Link> 
+           <button className='bg-blue-400 text-white  rounded-full  hover:brightness-95 transition-all duration-200 w-60 h-9 shadow-md hidden xl:inline'>
+              Sign In
+           </button>
+    </div>
+  )
+}
+
+export default Sidebar
